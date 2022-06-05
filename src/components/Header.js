e1181edc5af6c391e78999400661ffac865b954e
angelpulido96 <angel_akamaru@hotmail.com>
2022-06-04 18:41:04 -0600

    Se agrega el proyecto

---
+++ src/components/Header.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { 
+    Badge,
+    Container, 
+    FormControl,
+    Nav, 
+    Navbar,
+    Dropdown, 
+    Button
+} from 'react-bootstrap'
+import { AiFillDelete } from 'react-icons/ai';
+import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
+import { CartState } from '../context/Context';
+
+const Header = () => {
+
+    const { 
+        state: { cart }, 
+        dispatch,
+        productDispatch
+    } = CartState();
+
+  return (
+    <Navbar bg='dark' variant='dark' style={{height: 80 }}>
+        <Container>
+            <Navbar.Brand>
+                <Link to='/'>Shopping Cart</Link>
+            </Navbar.Brand>
+            <Navbar.Text className='search'>
+                <FormControl
+                    style={{width: 500}}
+                    type='search'
+                    placeholder='Search a Product...'
+                    className='m-auto'
+                    aria-label="Search"
+                    onChange={(e) => {
+                        productDispatch({
+                            type: 'FILTER_BY_SEARCH',
+                            payload: e.target.value,
+                        })
+                    }}
+                />
+            </Navbar.Text>
+            <Nav>
+                <Dropdown>
+                    <Dropdown.Toggle variant='success'>
+                        <FaShoppingCart color='white' fontSize='25px'/>
+                        <Badge bg='Success'>{cart.length}</Badge>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu style={{ minWidth: 370}}>
+                        { cart.length > 0 ? (
+                                <>
+                                    {
+                                        cart.map(prod => (
+                                            <span className='cartitem' key={prod.id}>
+                                                <img
+                                                    src={ prod.image}
+                                                    className='cartItemImg'
+                                                    alt={prod.name}
+                                                />
+                                                <div className='cartItemDetail'>
+                                                    <span>{prod.name}</span>
+                                                    <span>${prod.price}</span>
+                                                </div>
+                                                <AiFillDelete
+                                                    fontSize='20px'
+                                                    style={{cursor: 'pointer'}}
+                                                    onClick={() => 
+                                                        dispatch({
+                                                            type: 'REMOVE_FROM_CART',
+                                                            payload: prod,
+                                                        })
+                                                    }
+                                                />
+                                            </span>
+                                        ))}
+                                        <Link to='/cart'>
+                                            <Button style={{ width: '95%', margin: '0 10px'}}>
+                                                Go To Cart
+                                            </Button>
+                                        </Link>
+                                </>
+                            ) : (
+
+                                <span style={{padding: 10}}>Cart is Empty!</span>
+                            )
+                        }
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
